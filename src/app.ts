9d048b11245a6556e5a28de90e62ba4e4f02ecff
dosjcq <nagibin.artyom1999@gmail.com>
2022-03-21 00:30:52 +0300

    feat: add OOP rendering

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,31 @@
-// Code goes here!
+class ProjectInput {
+  templateElemnt: HTMLTemplateElement;
+  hostElemnt: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElemnt = document.getElementById(
+      'project-input',
+    )! as HTMLTemplateElement;
+    this.hostElemnt = document.getElementById(
+      'app',
+    )! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElemnt.content,
+      true,
+    );
+    this.element =
+      importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElemnt.insertAdjacentElement(
+      'afterbegin',
+      this.element,
+    );
+  }
+}
+
+const prjInput = new ProjectInput();
