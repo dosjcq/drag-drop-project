cc24dab875d28d52efdf2a32468703fdb5e9944c
dosjcq <nagibin.artyom1999@gmail.com>
2022-03-26 18:34:03 +0300

    refactor: split code with namespaces

---
+++ src/state/project-state.ts
@@ -0,0 +1,58 @@
+namespace App {
+  // Project State Managment
+  type Listener<T> = (items: T[]) => void;
+
+  class State<T> {
+    protected listeners: Listener<T>[] = [];
+
+    addListener(listenerFn: Listener<T>) {
+      this.listeners.push(listenerFn);
+    }
+  }
+
+  export class ProjectState extends State<Project> {
+    private protects: Project[] = [];
+    private static instance: ProjectState;
+
+    private constructor() {
+      super();
+    }
+
+    static getInstance() {
+      if (this.instance) {
+        return this.instance;
+      }
+      this.instance = new ProjectState();
+      return this.instance;
+    }
+
+    addProject(title: string, description: string, numberOfPeople: number) {
+      const newProject = new Project(
+        Math.random().toString(),
+        title,
+        description,
+        numberOfPeople,
+        ProjectStatus.Active,
+      );
+      this.protects.push(newProject);
+      this.updateListeners();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+      const project = this.protects.find((prj) => prj.id === projectId);
+
+      if (project && project.status !== newStatus) {
+        project.status = newStatus;
+        this.updateListeners();
+      }
+    }
+
+    private updateListeners() {
+      for (const listenerFn of this.listeners) {
+        listenerFn(this.protects.slice());
+      }
+    }
+  }
+
+  export const projectState = ProjectState.getInstance();
+}
