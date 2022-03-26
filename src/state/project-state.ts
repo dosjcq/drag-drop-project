d91147a687d2c75826c98f4941258fad284b2c60
dosjcq <nagibin.artyom1999@gmail.com>
2022-03-26 19:18:44 +0300

    refactor: replace namespaces with modules

--- src/state/project-state.ts
+++ src/state/project-state.ts
@@ -1,58 +1,58 @@
-namespace App {
-  // Project State Managment
-  type Listener<T> = (items: T[]) => void;
+import { Project, ProjectStatus } from '../models/project.js';
 
-  class State<T> {
-    protected listeners: Listener<T>[] = [];
+// Project State Managment
+type Listener<T> = (items: T[]) => void;
 
-    addListener(listenerFn: Listener<T>) {
-      this.listeners.push(listenerFn);
-    }
+class State<T> {
+  protected listeners: Listener<T>[] = [];
+
+  addListener(listenerFn: Listener<T>) {
+    this.listeners.push(listenerFn);
+  }
+}
+
+export class ProjectState extends State<Project> {
+  private protects: Project[] = [];
+  private static instance: ProjectState;
+
+  private constructor() {
+    super();
   }
 
-  export class ProjectState extends State<Project> {
-    private protects: Project[] = [];
-    private static instance: ProjectState;
-
-    private constructor() {
-      super();
-    }
-
-    static getInstance() {
-      if (this.instance) {
-        return this.instance;
-      }
-      this.instance = new ProjectState();
+  static getInstance() {
+    if (this.instance) {
       return this.instance;
     }
+    this.instance = new ProjectState();
+    return this.instance;
+  }
 
-    addProject(title: string, description: string, numberOfPeople: number) {
-      const newProject = new Project(
-        Math.random().toString(),
-        title,
-        description,
-        numberOfPeople,
-        ProjectStatus.Active,
-      );
-      this.protects.push(newProject);
+  addProject(title: string, description: string, numberOfPeople: number) {
+    const newProject = new Project(
+      Math.random().toString(),
+      title,
+      description,
+      numberOfPeople,
+      ProjectStatus.Active,
+    );
+    this.protects.push(newProject);
+    this.updateListeners();
+  }
+
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.protects.find((prj) => prj.id === projectId);
+
+    if (project && project.status !== newStatus) {
+      project.status = newStatus;
       this.updateListeners();
-    }
-
-    moveProject(projectId: string, newStatus: ProjectStatus) {
-      const project = this.protects.find((prj) => prj.id === projectId);
-
-      if (project && project.status !== newStatus) {
-        project.status = newStatus;
-        this.updateListeners();
-      }
-    }
-
-    private updateListeners() {
-      for (const listenerFn of this.listeners) {
-        listenerFn(this.protects.slice());
-      }
     }
   }
 
-  export const projectState = ProjectState.getInstance();
+  private updateListeners() {
+    for (const listenerFn of this.listeners) {
+      listenerFn(this.protects.slice());
+    }
+  }
 }
+
+export const projectState = ProjectState.getInstance();
