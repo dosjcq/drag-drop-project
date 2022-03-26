eb9f09883878e2fb1ca6f88a985723958eb64f14
dosjcq <nagibin.artyom1999@gmail.com>
2022-03-26 14:22:06 +0300

    feat: finish drag and drop

--- src/app.ts
+++ src/app.ts
@@ -63,6 +63,19 @@ class ProjectState extends State<Project> {
       ProjectStatus.Active,
     );
     this.protects.push(newProject);
+    this.updateListeners();
+  }
+
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.protects.find((prj) => prj.id === projectId);
+
+    if (project && project.status !== newStatus) {
+      project.status = newStatus;
+      this.updateListeners();
+    }
+  }
+
+  private updateListeners() {
     for (const listenerFn of this.listeners) {
       listenerFn(this.protects.slice());
     }
@@ -215,8 +228,13 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
     }
   }
 
+  @autobind
   dropHandler(event: DragEvent): void {
-    console.log(event.dataTransfer!.getData('text/plain'));
+    const prjId = event.dataTransfer!.getData('text/plain');
+    projectState.moveProject(
+      prjId,
+      this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished,
+    );
   }
 
   @autobind
